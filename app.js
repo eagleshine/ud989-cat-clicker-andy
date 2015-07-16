85191849568da660af1b2e5990944210769bfa92
Huibing Yin <huibingy@yahoo-inc.com>
2015-07-16 01:36:00 -0500

    First Model-View-Octopus version.

--- app.js
+++ app.js
@@ -1,36 +1,116 @@
+$(function() {
+  var model = {
+    init: function() {
+      this.data = {
+        cats: [
+          {
+            name: "cat_picture1.jpg",
+            count: 0
+          }, {
+            name: "cat_picture2.jpeg",
+            count: 0
+          }, {
+            name: "cat_picture3.jpeg",
+            count: 0
+          }, {
+            name: "cat_picture4.jpeg",
+            count: 0
+          }, {
+            name: "cat_picture5.jpeg",
+            count: 0
+          }
+        ],
+        currentCat: 0
+      };
+    },
+    getAllCats: function() {
+      return this.data.cats;
+    },
+    setCurrentCat: function(index) {
+      this.data.currentCat = index;
+    },
+    getCurrentCat: function() {
+      return this.data.cats[this.data.currentCat];
+    }
+  };
 
-var cats = $(".cat");
-var buttons = $("button");
 
-function hideAllCats(){
-	for (var i=0; i<cats.length; i++){
-		$(cats[i]).hide();
-	}
-}
+  var octopus = {
+    incrementCount: function() {
+      model.getCurrentCat().count += 1;
+    },
+    getCatName: function() {
+      return model.getCurrentCat().name;
+    },
+    getCatCount: function() {
+      return model.getCurrentCat().count;
+    },
+    getCats: function() {
+      return model.getAllCats();
+    },
+    getCatsNumber: function() {
+      return model.getAllCats().length;
+    },
+    setCat: function(index) {
+      model.setCurrentCat(index);
+    },
+    init: function() {
+      model.init();
+      listView.init();
+      catView.init();
+    },
+    refreshCat: function() {
+      catView.render();
+    }
+  };
 
-function bindButtonToCat(idNumber){
-	$("#button"+idNumber).click(function(){
-		hideAllCats();
-		$("#cat"+idNumber).show();
-	})
-}
 
-function bindCounterToCat(idNumber){
-	var cat = "#cat"+idNumber
-	$(cat).click(function(){
-		var count = $(cat+" > .counter").text();
-		count = parseInt(count) + 1;
-		$(cat+" > .counter").text(count);
-	})
-}
+  var listView = {
+    init: function() {
+      this.catlist = $('#catlist');
+      listView.render();
+    },
+    render: function() {
+      var htmlStr = '';
+      var cats = octopus.getCats();
+      for (var i=0; i < cats.length; i++) {
+        var name = cats[i].name.split('.')[0]
+        htmlStr += '<button id="button' + i +'">' + name + '</button>';
+      }
+      this.catlist.html(htmlStr);
+      for (var i=0; i < octopus.getCatsNumber(); i++) {
+        var button = "#button" + i;
+        $(button).click((function(index) {
+          return function() {
+            console.log("clicked button " + index);
+            octopus.setCat(index);
+            octopus.refreshCat();
+          }
+        })(i));
+      }
+    }
+  };
 
-for (var i=1; i<=buttons.length; i++){
-	bindButtonToCat(i);
-}
 
-for (var i=1; i<=cats.length; i++){
-	bindCounterToCat(i);
-}
+  var catView = {
+    init: function() {
+      this.cat = $('.cat');
+      this.cat.click(function() {
+        octopus.incrementCount();
+        catView.render();
+      })
+      catView.render();
+    },
+    render: function() {
+      var htmlStr = '';
+      htmlStr += '<span class="counter">' + octopus.getCatCount() + '</span> clicks' +
+        '<br>' +
+        '<img class="clicker" src="' + octopus.getCatName() + '">'
+        ;
+      this.cat.html(htmlStr);
+    }
+  };
 
-hideAllCats();
-$("#cat1").show();
+  octopus.init();
+});
+
